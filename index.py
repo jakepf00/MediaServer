8721c3d7308eb52dc4f7f5a1918fc820ba592055
Jacob Pfeifer <jakepf00@gmail.com>
2024-04-27 09:20:33 -0500

    Filter tracks list

--- index.py
+++ index.py
@@ -16,18 +16,27 @@ cur = con.cursor()
 doc, tag, text = Doc().tagtext()
 
 # HTML definition
-with tag('h1'):
-    text('Media Server')
-if 'song' in queries:
-	with tag('audio', controls=''):
-		curSong = cur.execute("SELECT * FROM files WHERE id={0}".format(queries['song'])).fetchone()
-		doc.stag('source', src='{0}'.format(str(curSong[1])))
-with tag('h2'):
-    text('Tracks')
-for row in cur.execute("SELECT * FROM files").fetchall():
-	with tag('ul'):
-		with tag('a', href='index.py?song='+str(row[0])):
-			text(row[1])
+doc.stag('!DOCTYPE', html='')
+with tag('html'):
+    with tag('head'):
+        doc.stag('link', rel='stylesheet', href='index.css')
+        with tag('script', src='index.js'):
+            text('')
+    with tag('body'):
+        with tag('h1'):
+            text('Media Server')
+        if 'song' in queries:
+            with tag('audio', controls=''):
+                curSong = cur.execute("SELECT * FROM files WHERE id={0}".format(queries['song'])).fetchone()
+                doc.stag('source', src='{0}'.format(str(curSong[1])))
+        with tag('h2'):
+            text('Tracks')
+        doc.stag('input', type="text", id="trackFilterInput", onkeyup="filterTracks()", plackholder="Search for track...")
+        with tag('ul', id='tracksUL'):
+            for row in cur.execute("SELECT * FROM files").fetchall():
+                with tag('li'):
+                    with tag('a', href='index.py?song='+str(row[0])):
+                        text(row[1])
 
 # Returning HTML results
 print(doc.getvalue())
